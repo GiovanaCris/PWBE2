9c5b3f61d91497f7f96dff328a2dfdbb4685c1ae
GiovanaCris <giovana.bp@outlook.com>
2025-02-25 11:12:30 -0300

    atividade 25-02

---
+++ aula2502/crud_exemplo/itens/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Item(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome 
+    
+    class Meta:
+        verbose_name_plural = "Itens"
