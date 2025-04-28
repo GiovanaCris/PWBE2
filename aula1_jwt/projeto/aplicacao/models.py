4e203cd25ccb05e20f9190c1b3e9255b4ca09c06
GiovanaCris <giovana.bp@outlook.com>
2025-04-28 08:33:10 -0300

    ativide jwt aplicacao e projeto

---
+++ aula1_jwt/projeto/aplicacao/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+#Modelo da tabela do banco de dados e definição dos campos
+class UsuarioDS16(AbstractUser):
+    idade = models.PositiveIntegerField()
+    animais = models.PositiveIntegerField()
+    telefone = models.CharField(max_length=255)
+    biografia = models.TextField()
+    endereco = models.CharField(max_length=255, null=True, blank=True)
+    escolaridade = models.CharField(max_length=255, null=True, blank=True)
+
+    REQUIRED_FIELDS = ['idade', 'animais', 'telefone', 'biografia', 'endereco', 'escolaridade']
+
+    def __str__(self): 
+        return self.username
