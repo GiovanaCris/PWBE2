a28367082dbcfbeb8f5b3dbb1e6ce4f2e361801d
Giovana <giovana.bp@outlook.com>
2025-04-28 08:22:36 -0300

    Delete aula1_jwt directory

--- aula1_jwt/projeto/aplicacao/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-#Configurações, ajustes e comportamentos do app
-class AplicacaoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'aplicacao'
--- aula1_jwt/projeto/aplicacao/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import UsuarioDS16 
-
-#Converte modelos Django em JSON
-class UsuarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UsuarioDS16
-        fields = '__all__'
